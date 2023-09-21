251bf0e1c341e779efccae2a3eb3ffc15ac9928d
Micah Sherr <msherr@cs.georgetown.edu>
2023-09-21 14:46:17 +0000

    added another example

---
+++ 2023-09-21/StaticMembers.java
@@ -0,0 +1,29 @@
+public class StaticMembers {
+    
+    public static int count1 = 0;
+    public int count2 = 0;
+
+    public void increment() {
+        count1++;
+        count2++;
+    }
+
+    public static void printCount1() {
+        System.out.println("count1 = " + count1);
+        // System.out.println("count2 = " + count2); // ERROR! Why?!
+    }
+
+    public static void main(String args[]) {
+        StaticMembers s1 = new StaticMembers();
+        StaticMembers s2 = new StaticMembers();
+
+        s1.increment();
+        s2.increment();
+
+        System.out.println("s1.count1 = " + s1.count1);
+        System.out.println("s1.count2 = " + s1.count2);
+
+        System.out.println("s2.count1 = " + s2.count1);
+        System.out.println("s2.count2 = " + s2.count2);
+    }
+}
