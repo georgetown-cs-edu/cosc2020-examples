a67b6fc6e2f6db71713235e8468847193a4d25cd
Micah Sherr <msherr@cs.georgetown.edu>
2023-11-01 21:31:31 -0400

    fomatting changes

--- 2023-11-01/CapitalizeServerRunnable.java
+++ 2023-11-01/CapitalizeServerRunnable.java
@@ -2,22 +2,20 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-
 import java.net.Socket;
 
-
 public class CapitalizeServerRunnable implements Runnable {
-  
+
   int clientNumber;
   Socket client;
   PrintWriter out;
   BufferedReader in;
 
-  public CapitalizeServerRunnable( int n, Socket s ) throws IOException {
+  public CapitalizeServerRunnable(int n, Socket s) throws IOException {
     clientNumber = n;
     client = s;
-    out = new PrintWriter( s.getOutputStream(), true);
-    in = new BufferedReader( new InputStreamReader( s.getInputStream() ) );
+    out = new PrintWriter(s.getOutputStream(), true);
+    in = new BufferedReader(new InputStreamReader(s.getInputStream()));
   }
 
   public void run() {
@@ -26,12 +24,12 @@ public class CapitalizeServerRunnable implements Runnable {
       out.println("Type \".\" to disconnect");
 
       boolean done = false;
-      while ( !done ) {
+      while (!done) {
         String input = in.readLine();
-        if ( input == null || input.equals(".") ) {
+        if (input == null || input.equals(".")) {
           done = true;
         }
-        if ( input != null ) {
+        if (input != null) {
           String output = input.toUpperCase();
           out.println(output);
           System.out.println("Sent \"" + output + "\" to client " + clientNumber);
@@ -40,11 +38,13 @@ public class CapitalizeServerRunnable implements Runnable {
     } // End try
     catch (IOException e) {
       System.out.println("Error with client " + clientNumber);
-      System.out.println("Exception: " + e.toString() );
+      System.out.println("Exception: " + e.toString());
     } finally {
       System.out.println("Closing connection to client " + clientNumber);
-      try { client.close(); }
-      catch (Exception e) { }
+      try {
+        client.close();
+      } catch (Exception e) {
+      }
     }
-  } // End run 
+  } // End run
 }
