f1534e067a551b54fd3739f469fd0b92451ec128
Micah Sherr <msherr@cs.georgetown.edu>
2023-11-14 09:42:58 -0500

    hello world GUI

--- 2023-11-14/HelloWorldGUI.java
+++ 2023-11-14/HelloWorldGUI.java
@@ -1,10 +1,13 @@
-import java.awt.*;
+import java.awt.FlowLayout;
 import java.awt.event.*;
-
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JTextArea;
 
 
-public class HelloWorldGUI extends Frame {
+public class HelloWorldGUI  {
+
+    private JTextArea ta;
 
     class MyFocusListener implements FocusListener {
         private String id;
@@ -14,21 +17,21 @@ public class HelloWorldGUI extends Frame {
         }
 
         public void focusGained(FocusEvent e) {
+            ta.setText(id + ": Focus Gained");
             System.out.println(id + ": Focus Gained");
         }
 
         public void focusLost(FocusEvent e) {
+            ta.setText(id + ": Focus Lost");
             System.out.println(id + ": Focus Lost");
         }
     }
 
     class MyMouseListener extends MouseAdapter {
         private String id;
-        private JTextArea ta;
 
-        public MyMouseListener(String id, JTextArea ta) {
+        public MyMouseListener(String id) {
             this.id = id;
-            this.ta = ta;
         }
 
         @Override
@@ -39,32 +42,34 @@ public class HelloWorldGUI extends Frame {
     }
 
     public HelloWorldGUI() {
-        JTextArea ta = new JTextArea("This is a text area");
+        JFrame frame = new JFrame("Fun with Buttons");
+
+        ta = new JTextArea("This is a text area");
         ta.setSize(150, 150);
         ta.setColumns(20);
         ta.setEditable(false);
 
-        Button btn1 = new Button("Button1");
+        JButton btn1 = new JButton("Button1");
         btn1.setBounds(50, 50, 50, 50);
-        btn1.addMouseListener(new MyMouseListener("button1", ta));
+        btn1.addMouseListener(new MyMouseListener("button1"));
         btn1.addFocusListener(new MyFocusListener("button1"));
-        add(btn1);
-        Button btn2 = new Button("Button2");
+        frame.add(btn1);
+        JButton btn2 = new JButton("Button2");
         btn2.setBounds(50, 50, 50, 50);
-        btn2.addMouseListener(new MyMouseListener("button2", ta));
+        btn2.addMouseListener(new MyMouseListener("button2"));
         btn2.addFocusListener(new MyFocusListener("button2"));
-        add(btn2);
+        frame.add(btn2);
 
-        add(ta);
+        frame.add(ta);
 
-        setSize(350, 350);
-        setTitle("This is my First AWT/Swing example");
-        setLayout(new FlowLayout());
-        setVisible(true);
+        frame.setSize(350, 350);
+        frame.setTitle("This is my first Swing example");
+        frame.setLayout(new FlowLayout());
+        frame.setVisible(true);
 
-        addWindowListener(new WindowAdapter() {
+        frame.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent we) {
-                dispose();
+                frame.dispose();
             }
         });
     }
