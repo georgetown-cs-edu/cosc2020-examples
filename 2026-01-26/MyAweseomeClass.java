1a622cfb26fc31db765a2fc0fe8a3de3c71111e2
Micah Sherr <msherr@cs.georgetown.edu>
2026-01-28 00:38:08 +0000

    renamed some stuff

--- 2026-01-26/MyAweseomeClass.java
+++ 2026-01-26/MyAweseomeClass.java
@@ -10,15 +10,12 @@ public class MyAweseomeClass {
 
         String s1 = "Hello world";
         String s2 = "Wuzzup?";
-        Integer x = Integer.valueOf(561);
 
         myVector.add(s1);
         myVector.add(s2);
-        myVector.add(x);
 
-
-        for( Object o : myVector ) {
-            System.out.println( o );
+        for( String s : myVector ) {
+            System.out.println( s );
         }
 
     }
---
+++ 2026-01-26/MyDemo.java
@@ -0,0 +1,9 @@
+public class MyDemo {
+    
+    public static void main( String args[] ) {
+        for (int i = 0; i < 10; i++ ) {
+            System.out.println(i);
+        }
+    }
+
+}
